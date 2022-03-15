fc01a71e28ffdb8757967dd20a619a14e8c13dfb
cmizell186 <chrislmiz311@gmail.com>
2022-03-15 09:36:21 -0700

    working on modal WIP

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,15 @@
 window.addEventListener("load", (event)=>{
     console.log("hello from javascript!")
+    const openSignIn = document.getElementById("sign-in-open");
+    const signInContainer = document.querySelector(".sign-in-container");
+    // const closeSignIn = document.getElementById("sign-in-close")
+
+    openSignIn.addEventListener('click',() =>{
+        signInContainer.classList.add('show');
+    })
+    signInContainer.addEventListener('click', (e) =>{
+        const inputChildren = Array.from(signInContainer.children)
+        // console.log(inputChildren);
+        signInContainer.classList.remove('show');
+    })
 })
