745eeb23aad92061accbee01ef0fdbfbc000ed4c
tristan <thuckabee.work@gmail.com>
2022-03-15 14:05:12 -0500

    Disabled Header-text Selection

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,11 +1,18 @@
 window.addEventListener("load", (event) => {
     console.log("hello from javascript!")
 
-    //Sign In Things
+    //HEADER
+    const header = document.getElementsByTagName('header')[0];
     const signInToggle = document.getElementById("sign-in-open");
     const signUpToggle = document.getElementById("sign-up-open");
     const signInContainer = document.querySelector(".sign-in-container");
     const signUpContainer = document.querySelector(".sign-up-container");
+
+    const disableselect = (e) => {  
+      return false  
+    }  
+    header.onselectstart = disableselect  
+    header.onmousedown = disableselect
 
     signInToggle.addEventListener('click',() =>{
         console.log('ClassList: ', signInContainer.classList.value)
