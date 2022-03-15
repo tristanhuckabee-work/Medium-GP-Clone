924368b3821a3b60b37658a3286c9b41f638de8c
cmizell186 <chrislmiz311@gmail.com>
2022-03-15 13:07:09 -0700

    finished most styling of modal

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,17 +1,16 @@
 window.addEventListener("load", (event) => {
     console.log("hello from javascript!")
-
     //Sign In Things
     const signInToggle = document.getElementById("sign-in-open");
     const signInContainer = document.querySelector(".sign-in-container");
 
-    signInToggle.addEventListener('click',() =>{
+    signInToggle.addEventListener('click',(e) =>{
+        e.preventDefault()
         console.log('ClassList: ', signInContainer.classList.value)
         if ( !signInContainer.classList.value.includes('show') ) {
           signInContainer.classList.add('show');
         } else {
           signInContainer.classList.remove('show');
         }
-
     })
 })
