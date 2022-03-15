ff503c85bfd58bd45fb28159bd86d8dbdf6440d2
tristan <thuckabee.work@gmail.com>
2022-03-15 14:24:07 -0500

    Modal and Header Fixes

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -8,29 +8,30 @@ window.addEventListener("load", (event) => {
     const signInContainer = document.querySelector(".sign-in-container");
     const signUpContainer = document.querySelector(".sign-up-container");
 
-    const disableselect = (e) => {  
-      return false  
-    }  
+    const disableselect = e => false;  
+
     header.onselectstart = disableselect  
     header.onmousedown = disableselect
 
     signInToggle.addEventListener('click',() =>{
-        console.log('ClassList: ', signInContainer.classList.value)
+
         if ( !signInContainer.classList.value.includes('show') ) {
           if (signUpContainer.classList.value.includes('show')) {
             signUpContainer.classList.remove('show');
           }
+
           signInContainer.classList.add('show');
         } else {
           signInContainer.classList.remove('show');
         }
     })
+
     signUpToggle.addEventListener('click',() =>{
-      console.log('ClassList: ', signUpContainer.classList.value)
       if ( !signUpContainer.classList.value.includes('show') ) {
         if (signInContainer.classList.value.includes('show')) {
           signInContainer.classList.remove('show');
         }
+        
         signUpContainer.classList.add('show');
       } else {
         signUpContainer.classList.remove('show');
