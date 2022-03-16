b473d67ecea0bc1ff266d7480abf9ed1fcfce009
mendezangel <id.angelmendez@gmail.com>
2022-03-16 15:06:40 -0700

    finished adding associations

--- db/models/user.js
+++ db/models/user.js
@@ -9,6 +9,35 @@ module.exports = (sequelize, DataTypes) => {
   User.associate = function (models) {
     // associations can be defined here
     User.hasMany(models.Record, { foreignKey: 'userId' });
+    User.belongsToMany(models.User, {
+      through: 'Follow',
+      otherKey: 'userId',
+      foreignKey: 'followerId',
+      as: 'followings'
+    });
+    User.belongsToMany(models.User, {
+      through: 'Follow',
+      otherKey: 'followerId',
+      foreignKey: 'userId',
+      as: 'followers'
+    });
+    User.hasMany(models.Applaud, { foreignKey: 'userId' });
+    User.hasMany(models.Comment, { foreignKey: 'userId' });
   };
   return User;
 };
+// This should get all of the users following the user whose userId you pass in
+// const user = await User.findByPk(userId, {
+//     include: [{
+//         model: User,
+//         as: 'followers'
+//     }]
+// }
+// This should get all users that the user with an id of userId is following
+// const user = await User.findByPk(userId, {
+//     include: [{
+//         model: User,
+//         as: 'followings'
+//     }]
+// }
+// Both queries result in a User object with a nested array of relevant users.
