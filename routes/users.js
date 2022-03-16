8ace260de4b7c8e6d05dbdab58c9ee0ae0b6211b
edwin carrillo <edwincarrillo9@outlook.com>
2022-03-16 12:17:23 -0400

    seperate navbar pug, skeleton users page

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const db = require('../db/models')
 const bcrypt = require('bcryptjs');
 const { check, validationResult } = require('express-validator');
 const { csrfProtection, asyncHandler } = require('./utils');
@@ -7,8 +8,14 @@ const { requireAuth, restoreUser, logoutUser } = require('../auth');
 
 
 /* GET users listing. */
-router.get('/', csrfProtection, function (req, res, next) {
-  res.send('respond with a resource');
+router.get('/:id', csrfProtection, (req, res, next) => {
+  if(res.locals.authenticated){
+    const pk = req.session.auth.userId
+    console.log(pk)
+    res.render('users', { pk })
+  }else{
+    res.redirect('/');
+  }
 });
 
 module.exports = router;
