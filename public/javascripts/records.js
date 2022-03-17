726d3d39e7091f76c77ebb0dc3fbdff12f043f07
cmizell186 <chrislmiz311@gmail.com>
2022-03-17 16:16:48 -0700

    got backend to work i think maybe

--- public/javascripts/records.js
+++ public/javascripts/records.js
@@ -1,14 +1,9 @@
-window.addEventListener("load",(e) =>{
+window.addEventListener("load", (e) => {
 
-const newComment = document.querySelector('.comment-submit');
-const commentArea = document.querySelector('.comment')
-newComment.addEventListener('click', async(e) =>{
-    // e.preventDefault()
-    console.log(commentArea.value);
-    const postId = e.target.value
-    console.log(postId);
-    // const res = await fetch()
-
-})
+    const newComment = document.querySelector('.comment-submit');
+    newComment.addEventListener('click',(e) =>{
+        e.preventDefault();
+        
+    })
 
 })
--- routes/records.js
+++ routes/records.js
@@ -97,13 +97,7 @@ router.get('/:id', csrfProtection, requireAuth,asyncHandler(async(req,res) =>{
       },
       order: [['id', 'DESC']]
   })
-
-  if(record.userId !== req.session.auth.userId){
-    res.redirect("/records")
-  } else {
-    console.log(comments[0].id, "HEY THERE CHECK THIS OUT COMMENTS ID");
-    res.render('recordId', { record, comments, csrfToken: req.csrfToken()})
-  }
+  res.render('recordId', { record, comments, csrfToken: req.csrfToken()})
 }))
 
 // validator for commments
@@ -119,12 +113,10 @@ router.post('/:id/comments/:commentsId',
 commentsVal,
 csrfProtection,
 requireAuth,
-handleValidationErrors,
 asyncHandler(async(req,res)=>{
-  const id = req.params.id;
   const {comment} = req.body;
-  
-
+  res.json({description: comment}).end();
+  console.log('sending post request')
 }))
 
 module.exports = router
