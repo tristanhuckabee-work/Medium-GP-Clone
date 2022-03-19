c2921600e6a7c0a30eb8ad5866f87e60837f9b78
Edwin Carrillo <69633370+edwincarr@users.noreply.github.com>
2022-03-19 19:02:59 -0400

    Update records.js

--- routes/records.js
+++ routes/records.js
@@ -89,7 +89,6 @@ router.post('/:id/edit', csrfProtection, recordVal, requireAuth, asyncHandler(as
     res.redirect('/records')
   } else {
     const errors = validatorErrors.array().map(error => error.msg);
-    console.log(id)
     res.render('editRecord', {
       title,
       description,
